4946bfcd3fa8d9f30cab74e42a8c8280b567ec7a
Nikolay <nik.pavlovskiy.1997@mail.ru>
2023-03-08 19:16:11 +0300

    Make task 3 for hom 2

--- Homework/Hom_2/Fyncshion.java
+++ Homework/Hom_2/Fyncshion.java
@@ -1,5 +1,19 @@
 package Homework.Hom_2;
 
 public class Fyncshion {
-    
+    public static void PaliandrText(String tex) {
+        int sizeTex = tex.length();
+        char left;
+        char right;
+        boolean flag = true;
+        for (int i = 0; i < tex.length() / 2; i++) {
+            left = tex.charAt(i);
+            right = tex.charAt(sizeTex - 1 - i);
+            if (left != right) {
+                flag = false;
+                break;
+            }        
+        }
+        System.out.println(flag);
+    }
 }
--- Homework/Hom_2/Obj_3.java
+++ Homework/Hom_2/Obj_3.java
@@ -1,7 +1,18 @@
 package Homework.Hom_2;
 
+import java.util.Scanner;
+
 public class Obj_3 {
     public static void main(String[] args) {
+        try {
+            Scanner scan = new Scanner(System.in, "Cp866");
+        System.out.println("Введите текст: ");
+        String text = scan.nextLine();
+        scan.close();
+        Fyncshion.PaliandrText(text);
+        } catch (Exception e) {
+            System.out.println("Что-то пошло не так, попробуйте ещё раз");
+        }
         
     }
 }
